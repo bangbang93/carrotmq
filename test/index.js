ecc5b3748a8cf0a85a5d0b54b0f16d0ae33fd561
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-08 08:38:08 +0000

    Deploy bangbang93/carrotmq to github.com/bangbang93/carrotmq.git:gh-pages

--- test/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by bangbang93 on 2017/8/24.
- */
-'use strict';
-// require('babel-register')
-require('ts-node/register/transpile-only')
-
-require('./test')
-require('./no-schema')
